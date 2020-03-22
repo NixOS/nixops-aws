dbaed62e8d98c455a12ae7bc2b8326d23a4e1e94
Benjamin Hipple <bhipple@protonmail.com>
2020-03-22 15:24:51 -0400

    py3-prep: fix filter with list comprehensions
    In py2, filter returned a list; in py3, it returns an iterator, which can't be indexed.
    
    Per upstream guidelines [1], while it is possible to just add a `list()` cast,
    it's more idiomatic to use a list comprehension for filtering. This is more
    pythonic regardless, and still py2 compatible.
    
    Partial progress towards #29
    
    [1] https://docs.python.org/3.0/whatsnew/3.0.html#views-and-iterators-instead-of-lists

--- nixopsaws/backends/ec2.py
+++ nixopsaws/backends/ec2.py
@@ -858,16 +858,14 @@ class EC2State(MachineState, nixopsaws.resources.ec2_common.EC2CommonState):
                     self.ssh_pinged = False
 
     def security_groups_to_ids(self, subnetId, groups):
-        sg_names = filter(lambda g: not g.startswith("sg-"), groups)
+        sg_names = [g for g in groups if not g.startswith("sg-")]
         if sg_names != [] and subnetId != "":
             self.connect_vpc()
             vpc_id = self._conn_vpc.get_all_subnets([subnetId])[0].vpc_id
-            groups = map(
-                lambda g: nixopsaws.ec2_utils.name_to_security_group(
-                    self._conn, g, vpc_id
-                ),
-                groups,
-            )
+            groups = [
+                nixopsaws.ec2_utils.name_to_security_group(self._conn, g, vpc_id)
+                for g in groups
+            ]
 
         return groups
 
