6ae512a447b7d06a44d7d8b50572198a1a24f4d2
Graham Christensen <graham@grahamc.com>
2020-05-12 17:35:32 -0400

    Finish up connect refactor

--- nixops_aws/resources/ebs_volume.py
+++ nixops_aws/resources/ebs_volume.py
@@ -61,13 +61,13 @@ class EBSVolumeState(nixops.resources.ResourceState, ec2_common.EC2CommonState):
     def resource_id(self):
         return self.volume_id
 
-    def connect(self, region):
+    def _connect(self, region):
         if self._conn:
             return self._conn
         self._conn = nixops_aws.ec2_utils.connect(region, self.access_key_id)
         return self._conn
 
-    def connect_boto3(self, region):
+    def _connect_boto3(self, region):
         if self._conn_boto3:
             return self._conn_boto3
         self._conn_boto3 = nixops_aws.ec2_utils.connect_ec2_boto3(
@@ -76,9 +76,8 @@ class EBSVolumeState(nixops.resources.ResourceState, ec2_common.EC2CommonState):
         return self._conn_boto3
 
     def _get_vol(self, config):
-        self.connect_boto3(config["region"])
         try:
-            _vol = self._connect_boto3().describe_volumes(VolumeIds=[config["volumeId"]])[
+            _vol = self._connect_boto3(config['region']).describe_volumes(VolumeIds=[config["volumeId"]])[
                 "Volumes"
             ][0]
         except botocore.exceptions.ClientError as error:
@@ -105,8 +104,6 @@ class EBSVolumeState(nixops.resources.ResourceState, ec2_common.EC2CommonState):
             raise Exception(
                 "please set ‘accessKeyId’, $EC2_ACCESS_KEY or $AWS_ACCESS_KEY_ID"
             )
-
-        self.connect(defn.config["region"])
 
         if self._exists():
             if self.region != defn.config["region"] or self.zone != defn.config["zone"]:
@@ -140,7 +137,7 @@ class EBSVolumeState(nixops.resources.ResourceState, ec2_common.EC2CommonState):
                 self._get_vol(defn.config)
             else:
                 if defn.config["size"] == 0 and defn.config["snapshot"] != "":
-                    snapshots = self._connect().get_all_snapshots(
+                    snapshots = self._connect(defn.config['region']).get_all_snapshots(
                         snapshot_ids=[defn.config["snapshot"]]
                     )
                     assert len(snapshots) == 1
@@ -162,7 +159,7 @@ class EBSVolumeState(nixops.resources.ResourceState, ec2_common.EC2CommonState):
                         "please set a zone where the volume will be created"
                     )
 
-                volume = self._connect().create_volume(
+                volume = self._connect(defn.config['region']).create_volume(
                     zone=defn.config["zone"],
                     size=defn.config["size"],
                     snapshot=defn.config["snapshot"],
@@ -187,13 +184,13 @@ class EBSVolumeState(nixops.resources.ResourceState, ec2_common.EC2CommonState):
         if self.state == self.STARTING or check:
             self.update_tags(self.volume_id, user_tags=defn.config["tags"], check=check)
             nixops_aws.ec2_utils.wait_for_volume_available(
-                self._conn, self.volume_id, self.logger, states=["available", "in-use"]
+                self._connect(self.region), self.volume_id, self.logger, states=["available", "in-use"]
             )
             self.state = self.UP
 
     def check(self):
         volume = nixops_aws.ec2_utils.get_volume_by_id(
-            self.connect(self.region), self.volume_id
+            self._connect(self.region), self.volume_id
         )
         if volume is None:
             self.state = self.MISSING
@@ -203,11 +200,11 @@ class EBSVolumeState(nixops.resources.ResourceState, ec2_common.EC2CommonState):
             return True
 
         if wipe:
-            log.warn("wipe is not supported")
+            self.warn("wipe is not supported")
 
-        self.connect(self.region)
+
         volume = nixops_aws.ec2_utils.get_volume_by_id(
-            self._conn, self.volume_id, allow_missing=True
+            self._connect(self.region), self.volume_id, allow_missing=True
         )
         if not volume:
             return True
