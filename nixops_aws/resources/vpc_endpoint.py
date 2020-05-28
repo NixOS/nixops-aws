db717eb0eef23b2eaa5985ed8b77d45d04c7649b
adisbladis <adisbladis@gmail.com>
2020-05-28 07:58:57 +0100

    nixops_aws/resources/vpc_endpoint.py: Fix flake8 errors

--- nixops_aws/resources/vpc_endpoint.py
+++ nixops_aws/resources/vpc_endpoint.py
@@ -7,8 +7,6 @@ from nixops.diff import Handler
 import nixops.util
 import nixops.resources
 from nixops_aws.resources.ec2_common import EC2CommonState
-from . import vpc, vpc_route_table
-import nixops_aws.ec2_utils
 from .vpc import VPCState
 from .vpc_route_table import VPCRouteTableState
 
@@ -80,8 +78,8 @@ class VPCEndpointState(nixops.resources.DiffEngineResourceState, EC2CommonState)
         return {
             r
             for r in resources
-            if isinstance(r, vpc.VPCState)
-            or isinstance(r, vpc_route_table.VPCRouteTableState)
+            if isinstance(r, VPCState)
+            or isinstance(r, VPCRouteTableState)
         }
 
     def realize_create_endpoint(self, allow_recreate):
