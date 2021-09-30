4aeef389777d9b7c8b3da1ab48f26bbd99582e0e
Rehno Lindeque <errantkid@gmail.com>
2021-09-30 08:58:58 -0400

    Fix escape sequences that needs to be doubly escaped

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == "__main__":
     config = nose.config.Config(plugins=nose.plugins.manager.DefaultPluginManager())
-    config.configure(argv=[sys.argv[0], "-e", "^coverage-tests\.py$"] + sys.argv[1:])
+    config.configure(argv=[sys.argv[0], "-e", "^coverage-tests\\.py$"] + sys.argv[1:])
     count = (
         nose.loader.defaultTestLoader(config=config)
         .loadTestsFromNames(["."])
@@ -15,7 +15,7 @@ if __name__ == "__main__":
             "--process-timeout=inf",
             "--processes=%d".format(count),
             "-e",
-            "^coverage-tests\.py$",
+            "^coverage-tests\\.py$",
         ]
         + sys.argv[1:]
     )
